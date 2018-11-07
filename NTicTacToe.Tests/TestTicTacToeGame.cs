99f5a9def00931dc269d5d972ee2ec6714be3650
Dylan Sprague <dylan.richard.sprague@gmail.com>
2018-11-07 00:32:13 -0500

    Start implementing TicTacToeGame.MakeMove()
    Have MakeMove() handle cases of already filled cell, empty cell that doesn't cause win

--- NTicTacToe.Tests/TestTicTacToeGame.cs
+++ NTicTacToe.Tests/TestTicTacToeGame.cs
@@ -28,5 +28,53 @@ namespace NTicTacToe.Tests
             var game = new TicTacToeGame();
             Assert.IsTrue(game.Board.All(cell => cell == CellState.Empty));
         }
+
+        [TestMethod]
+        public void MoveToFilledSquare_ShouldReturnError()
+        {
+            var game0 = new TicTacToeGame();
+            var (_, game1) = game0.MakeMove(0);
+            var (result, game2) = game1.MakeMove(0);
+            Assert.AreEqual(MoveResult.CellFilled, result);
+        }
+
+        [TestMethod]
+        public void XMoveToEmptySquare_ShouldFillCellWithX()
+        {
+            var cellNum = 0;
+            var game0 = new TicTacToeGame();
+            var (result, game1) = game0.MakeMove(cellNum);
+            Assert.AreEqual(MoveResult.WaitingForMove, result); // should this be in this test, or a separate one?
+            Assert.AreEqual(CellState.X, game1.Board[cellNum]);
+        }
+
+        [TestMethod]
+        public void OMoveToEmptySquare_ShouldFillCellWithO()
+        {
+            var xCellNum = 0;
+            var oCellNum = 1;
+            var game0 = new TicTacToeGame();
+            var (_, game1) = game0.MakeMove(xCellNum);
+            var (result, game2) = game1.MakeMove(oCellNum);
+            Assert.AreEqual(MoveResult.WaitingForMove, result); // should this be in this test, or a separate one?
+            Assert.AreEqual(CellState.O, game2.Board[oCellNum]);
+        }
+
+        [TestMethod]
+        public void SuccessfulXMove_ShouldMakeOCurrentPlayer()
+        {
+            var game0 = new TicTacToeGame();
+            var (_, game1) = game0.MakeMove(0);
+            Assert.AreEqual(Player.O, game1.CurrentPlayer);
+        }
+
+        [TestMethod]
+        public void SuccessfulOMove_ShouldMakeXCurrentPlayer()
+        {
+            var game0 = new TicTacToeGame();
+            var (_, game1) = game0.MakeMove(0);
+            var (_, game2) = game1.MakeMove(1);
+            Assert.AreEqual(Player.X, game2.CurrentPlayer);
+        }
     }
 }
--- NTicTacToe/Models/TicTacToeGame.cs
+++ NTicTacToe/Models/TicTacToeGame.cs
@@ -1,4 +1,8 @@
-﻿
+﻿// Represents a tic-tac-toe game's state
+// presents an immutable API, with MakeMove() returning a new game state and maintaining internal consistency
+// port of TypeScript tic-tac-toe game class from https://github.com/DylanSp/tic-tac-toe-react/blob/master/src/TicTacToeGame.ts
+
+using System;
 using System.Linq;
 
 namespace NTicTacToe.Models
@@ -24,19 +28,56 @@ namespace NTicTacToe.Models
         Empty
     }
 
+    public enum MoveResult
+    {
+        CellFilled,
+        WaitingForMove
+    }
+
     public class TicTacToeGame
     {
-        public Player CurrentPlayer { get; }
+        public Player CurrentPlayer { get; private set; }
 
-        public GameResult Result { get; }
+        public GameResult Result { get; private set; }
 
-        public CellState[] Board { get; }
+        public CellState[] Board { get; private set; }
 
         public TicTacToeGame()
         {
             CurrentPlayer = Player.X;
             Result = GameResult.Unfinished;
             Board = Enumerable.Repeat(CellState.Empty, 9).ToArray();
+        }
+
+        public (MoveResult result, TicTacToeGame updatedGame) MakeMove(int cellNum)
+        {
+            if (cellNum < 0 || cellNum > 8)
+            {
+                throw new Exception("Cell number is outside of the 0-8 range!");
+            }
+
+            var updatedGame = new TicTacToeGame();
+            updatedGame.CurrentPlayer = this.CurrentPlayer;
+            updatedGame.Result = this.Result;
+            Array.Copy(this.Board, updatedGame.Board, this.Board.Length);
+
+            if (updatedGame.Board[cellNum] != CellState.Empty)
+            {
+                return (MoveResult.CellFilled, updatedGame);
+            }
+
+            if (updatedGame.CurrentPlayer == Player.X)
+            {
+                updatedGame.Board[cellNum] = CellState.X;
+                updatedGame.CurrentPlayer = Player.O;
+            }
+            else
+            {
+                updatedGame.Board[cellNum] = CellState.O;
+                updatedGame.CurrentPlayer = Player.X;
+            }
+            
+            return (MoveResult.WaitingForMove, updatedGame);
         }
     }
 }
