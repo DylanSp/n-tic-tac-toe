2017e789f271390ea5dba421d6b9be32784e5161
Dylan Sprague <dsprague@docuphase.com>
2018-11-07 15:50:26 -0500

    Add generic IDataAdapter interface

---
+++ Interfaces/IDataAdapter.cs
@@ -0,0 +1,15 @@
+﻿namespace Interfaces
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+
+    public interface IDataAdapter<T> where T : IEntity
+    {
+        T Read(int id);
+        int Create();
+        void Update(T newData);
+        void Delete(int id);
+    }
+}
