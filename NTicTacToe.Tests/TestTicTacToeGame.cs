84205f6aac98951ebc9b008a318e808947a3ccdb
Dylan Sprague <dylan.richard.sprague@gmail.com>
2018-11-06 23:50:13 -0500

    Set up unit test project, create tests for game start

---
+++ NTicTacToe.Tests/TestTicTacToeGame.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NTicTacToe.Models;
+
+namespace NTicTacToe.Tests
+{
+    [TestClass]
+    public class TestTicTacToeGame
+    {
+        [TestMethod]
+        public void NewGame_ShouldStartWithPlayerX()
+        {
+            var game = new TicTacToeGame();
+            Assert.AreEqual(Player.X, game.CurrentPlayer);
+        }
+
+        [TestMethod]
+        public void NewGame_ShouldStartWithUnfinishedGameResult()
+        {
+            var game = new TicTacToeGame();
+            Assert.AreEqual(GameResult.Unfinished, game.Result);
+        }
+
+        [TestMethod]
+        public void NewGame_ShouldStartWithEmptyBoard()
+        {
+            var game = new TicTacToeGame();
+            Assert.IsTrue(game.Board.All(cell => cell == CellState.Empty));
+        }
+    }
+}
---
+++ NTicTacToe/Models/TicTacToeGame.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Linq;
+
+namespace NTicTacToe.Models
+{
+    public enum Player
+    {
+        X,
+        O
+    }
+
+    public enum GameResult
+    {
+        XWon,
+        OWon,
+        Drawn,
+        Unfinished
+    }
+
+    public enum CellState
+    {
+        X,
+        O,
+        Empty
+    }
+
+    public class TicTacToeGame
+    {
+        public Player CurrentPlayer { get; }
+
+        public GameResult Result { get; }
+
+        public CellState[] Board { get; }
+
+        public TicTacToeGame()
+        {
+            CurrentPlayer = Player.X;
+            Result = GameResult.Unfinished;
+            Board = Enumerable.Repeat(CellState.Empty, 9).ToArray();
+        }
+    }
+}
