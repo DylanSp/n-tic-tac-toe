cd17bc5d6c8951d16fb6e7e1eefc91a96f8da883
Dylan Sprague <dylan.richard.sprague@gmail.com>
2018-11-08 00:22:50 -0500

    Implement TTTDataAdapter.Read(), .Update()
    Also change (de)serialization to use strings, StringReader/StringWriter

--- Data/TTTDataAdapter.cs
+++ Data/TTTDataAdapter.cs
@@ -2,19 +2,22 @@
 using System.Data;
 using System.Data.SqlClient;
 using System.IO;
+using System.Xml;
 using System.Xml.Serialization;
 
 namespace Data
 {
     public class TTTDataAdapter : IDataAdapter<TicTacToeData>
     {
-        private const string ConnectionString = "Data Source=localhost;Initial Catalog=TicTacToe;Persist Security Info=True;User ID=sa;Password=D0cupPhase1!";
+        // private const string ConnectionString = "Data Source=localhost;Initial Catalog=TicTacToe;Persist Security Info=True;User ID=sa;Password=D0cupPhase1!";
+        private const string ConnectionString = "Data Source=localhost;Initial Catalog=TicTacToe;Persist Security Info=True;User ID=sa;Password=adminpass";
 
+        // can throw exceptions from opening connection, running query, serialization
         public int Create()
         {
             using (var connection = new SqlConnection(ConnectionString))
             {
-                var queryString = "INSERT GameData (GameState) OUTPUT inserted.Id VALUES @state";
+                var queryString = "INSERT GameData (GameState) OUTPUT inserted.Id VALUES (@state)";
                 var command = new SqlCommand(queryString, connection);
 
                 var stateParam = new SqlParameter("@state", SqlDbType.NVarChar);
@@ -29,9 +32,21 @@ namespace Data
 
         public void Delete(int id)
         {
-            throw new System.NotImplementedException();
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                var queryString = "DELETE FROM GameData WHERE Id = @id";
+                var command = new SqlCommand(queryString, connection);
+
+                var idParam = new SqlParameter("@id", SqlDbType.Int);
+                idParam.Value = id;
+                command.Parameters.Add(idParam);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
         }
 
+        // can throw exceptions from opening connection, running query, deserialization
         public TicTacToeData Read(int id)
         {
             using (var connection = new SqlConnection(ConnectionString))
@@ -42,37 +57,53 @@ namespace Data
                 var idParam = new SqlParameter("@id", SqlDbType.Int);
                 idParam.Value = id;
                 command.Parameters.Add(idParam);
-
-                // let exceptions propagate up
+                
                 connection.Open();
-                var returnData = (byte[])command.ExecuteScalar();
-                return DeserializeGameData(returnData);
+                var returnData = (string)command.ExecuteScalar();
+                var data = DeserializeGameData(returnData);
+
+                // need to update Id on C# side; otherwise, gets initialized to 0, ignoring value in DB
+                data.Id = id;
+                return data;
             }
         }
 
         public void Update(TicTacToeData newData)
         {
-            throw new System.NotImplementedException();
-        }
-
-        public static byte[] SerializeGameData(TicTacToeData data)
-        {
-            var serializer = new XmlSerializer(typeof(TicTacToeData));
-            using (var memStream = new MemoryStream())
+            using (var connection = new SqlConnection(ConnectionString))
             {
-                serializer.Serialize(memStream, data);
-                return memStream.ToArray();
+                var queryString = "UPDATE GameData SET GameState = @state WHERE Id = @id";
+                var command = new SqlCommand(queryString, connection);
+
+                var stateParam = new SqlParameter("@state", SqlDbType.NVarChar);
+                stateParam.Value = SerializeGameData(newData);
+                command.Parameters.Add(stateParam);
+
+                var idParam = new SqlParameter("@id", SqlDbType.Int);
+                idParam.Value = newData.Id; 
+                command.Parameters.Add(idParam);
+
+                connection.Open();
+                command.ExecuteNonQuery();
             }
         }
 
-        public static TicTacToeData DeserializeGameData(byte[] data)
+        public static string SerializeGameData(TicTacToeData data)
+        {
+            var serializer = new XmlSerializer(typeof(TicTacToeData));
+            using (var stringWriter = new StringWriter())
+            {
+                serializer.Serialize(stringWriter, data);
+                return stringWriter.ToString();
+            }
+        }
+
+        public static TicTacToeData DeserializeGameData(string data)
         {
             var deserializer = new XmlSerializer(typeof(TicTacToeData));
-            using (var memStream = new MemoryStream())
+            using (var reader = new StringReader(data))
             {
-                memStream.Write(data, 0, data.Length);
-                memStream.Seek(0, SeekOrigin.Begin);
-                return (TicTacToeData)deserializer.Deserialize(memStream);
+                return deserializer.Deserialize(reader) as TicTacToeData;
             }
         }
     }
--- NTicTacToe.Tests/TestTTTDataAdapter.cs
+++ NTicTacToe.Tests/TestTTTDataAdapter.cs
@@ -15,5 +15,18 @@ namespace NTicTacToe.Tests
             // Assert.AreEqual(data, roundTripped);
             Assert.IsTrue(data.Equals(roundTripped));
         }
+
+        // note - requires database connection!
+        [TestMethod]
+        public void TempTests()
+        {
+            var adapter = new TTTDataAdapter();
+            int createdId0 = adapter.Create();
+            int createdId1 = adapter.Create();
+            var gameData0 = adapter.Read(createdId0);
+            gameData0.CurrentPlayer = Player.O;
+            adapter.Update(gameData0);
+            adapter.Delete(createdId1);
+        }
     }
 }
