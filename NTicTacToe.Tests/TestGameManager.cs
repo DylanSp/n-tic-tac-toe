5050c1487a6f2372980d81f38a0c233e21df2b14
Dylan Sprague <dsprague@docuphase.com>
2018-11-09 15:03:00 -0500

    Have TestGameManager use TestInitialize for setup

--- NTicTacToe.Tests/TestGameManager.cs
+++ NTicTacToe.Tests/TestGameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Data;
+using Interfaces;
 using Managers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Types;
@@ -8,33 +9,37 @@ using Types;
 namespace NTicTacToe.Tests
 {
     [TestClass]
-    public class TestTicTacToeGame
+    public class TestGameManager
     {
+        private IGameManager game;
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            game = new GameManager(new TicTacToeData());
+        }
+
         [TestMethod]
         public void NewGame_ShouldStartWithPlayerX()
         {
-            var game = new GameManager();
             Assert.AreEqual(Player.X, game.GameData.CurrentPlayer);
         }
 
         [TestMethod]
         public void NewGame_ShouldStartWithUnfinishedGameResult()
         {
-            var game = new GameManager();
             Assert.AreEqual(GameResult.Unfinished, game.GameData.Result);
         }
 
         [TestMethod]
         public void NewGame_ShouldStartWithEmptyBoard()
         {
-            var game = new GameManager();
             Assert.IsTrue(game.GameData.Board.All(cell => cell == CellState.Empty));
         }
 
         [TestMethod]
         public void MoveToFilledSquare_ShouldReturnError()
         {
-            var game = new GameManager();
             game.MakeMove(0);
             var result = game.MakeMove(0);
             Assert.AreEqual(MoveResult.CellFilled, result);
@@ -44,7 +49,6 @@ namespace NTicTacToe.Tests
         public void XMoveToEmptySquare_ShouldFillCellWithX()
         {
             var cellNum = 0;
-            var game = new GameManager();
             var result = game.MakeMove(cellNum);
             Assert.AreEqual(MoveResult.WaitingForMove, result); // should this be in this test, or a separate one?
             Assert.AreEqual(CellState.X, game.GameData.Board[cellNum]);
@@ -55,7 +59,6 @@ namespace NTicTacToe.Tests
         {
             var xCellNum = 0;
             var oCellNum = 1;
-            var game = new GameManager();
             game.MakeMove(xCellNum);
             var result = game.MakeMove(oCellNum);
             Assert.AreEqual(MoveResult.WaitingForMove, result); // should this be in this test, or a separate one?
@@ -65,7 +68,6 @@ namespace NTicTacToe.Tests
         [TestMethod]
         public void SuccessfulXMove_ShouldMakeOCurrentPlayer()
         {
-            var game = new GameManager();
             game.MakeMove(0);
             Assert.AreEqual(Player.O, game.GameData.CurrentPlayer);
         }
@@ -73,7 +75,6 @@ namespace NTicTacToe.Tests
         [TestMethod]
         public void SuccessfulOMove_ShouldMakeXCurrentPlayer()
         {
-            var game = new GameManager();
             game.MakeMove(0);
             game.MakeMove(1);
             Assert.AreEqual(Player.X, game.GameData.CurrentPlayer);
@@ -82,7 +83,6 @@ namespace NTicTacToe.Tests
         [TestMethod]
         public void XWinningSequence_ShouldShowXAsWinningPlayer()
         {
-            var game = new GameManager();
             var results = new List<MoveResult>();
             var moveSequence = new int[] {0, 3, 1, 4, 2};
             foreach (var move in moveSequence)
@@ -97,7 +97,6 @@ namespace NTicTacToe.Tests
         [TestMethod]
         public void OWinningSequence_ShouldShowOAsWinningPlayer()
         {
-            var game = new GameManager();
             var results = new List<MoveResult>();
             var moveSequence = new int[] { 0, 3, 1, 4, 7, 5 };
             foreach (var move in moveSequence)
@@ -112,7 +111,6 @@ namespace NTicTacToe.Tests
         [TestMethod]
         public void MoveInCompletedGame_ShouldReturnError()
         {
-            var game = new GameManager();
             var results = new List<MoveResult>();
             var moveSequence = new int[] { 0, 3, 1, 4, 2, 8};
             foreach (var move in moveSequence)
@@ -126,7 +124,6 @@ namespace NTicTacToe.Tests
         [TestMethod]
         public void GameDrawingSequence_ShouldShowDrawnGame()
         {
-            var game = new GameManager();
             var results = new List<MoveResult>();
             var moveSequence = new int[] { 0, 3, 1, 4, 5, 2, 6, 7, 8 };
             foreach (var move in moveSequence)
