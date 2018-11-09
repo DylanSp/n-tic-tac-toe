84c0ec3dc862bb6635e8e3463c77466558303ab1
Dylan Sprague <dylan.richard.sprague@gmail.com>
2018-11-09 05:45:26 -0500

    Add DataManager to contain both game logic and interacting with DB

---
+++ Managers/DataManager.cs
@@ -0,0 +1,31 @@
+﻿using Data;
+using Interfaces;
+
+namespace Managers
+{
+    class DataManager
+    {
+        public GameManager GameManager { get; private set; }
+        private IDataAdapter<TicTacToeData> Adapter { get; set; }
+
+        // don't want to do this in constructor, because CreateOrUpdate() can throw
+        public void CreateAndSaveGame()
+        {
+            GameManager = new GameManager();
+            Adapter.CreateOrUpdate(GameManager.GameData);
+        }
+
+        public MoveResult AttemptAndSaveMove(int cellNum)
+        {
+            var result = GameManager.MakeMove(cellNum);
+            
+            // if move was successful, game state is changed, save it via adapter
+            if (result == MoveResult.WaitingForMove || result == MoveResult.GameFinished)
+            {
+                Adapter.CreateOrUpdate(GameManager.GameData);
+            }
+
+            return result;
+        }
+    }
+}
