94a967e6892739cfbe9d4d73ac853f8bf5d746ee
Dylan Sprague <dsprague@docuphase.com>
2018-11-16 15:43:09 -0500

    Add integration tests for TTTDataAdapter

--- NTicTacToe.Tests/TestTTTDataAdapter.cs
+++ NTicTacToe.Tests/TestTTTDataAdapter.cs
@@ -1,6 +1,7 @@
 ﻿using Data;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Configuration;
+using System.Data.SqlClient;
 using Types;
 
 namespace NTicTacToe.Tests
@@ -16,23 +17,135 @@ namespace NTicTacToe.Tests
             Assert.AreEqual(data, roundTripped);
         }
 
-        // note - requires database connection!
         [TestMethod, TestCategory("IntegrationTest")]
-        public void TempTests()
+        public void SaveThenRead_ShouldRoundTrip()
         {
+            // Arrange
             var connectionString = ConfigurationManager.ConnectionStrings["test"].ToString();
             var adapter = new TTTDataAdapter(connectionString);
-            var gameData0 = new TicTacToeData();
-            adapter.Save(gameData0);
-            var gameDataShouldEqual0 = adapter.Read(gameData0.Id);
-            Assert.AreEqual(gameData0, gameDataShouldEqual0);
+            var gameData = new TicTacToeData();
 
-            gameData0.CurrentPlayer = Player.O;
-            adapter.Save(gameData0);
+            // change gameData to make sure Read() isn't just constructing new TicTacToeData()
+            gameData.CurrentPlayer = Player.O;
 
-            var gameData1 = new TicTacToeData();
-            adapter.Save(gameData1);
-            adapter.Delete(gameData1.Id);
+            // Act
+            adapter.Save(gameData);
+            var readData = adapter.Read(gameData.Id);
+
+            // Assert
+            Assert.AreEqual(gameData, readData);
+        }
+
+        [TestMethod, TestCategory("IntegrationTest")]
+        public void SavingNewObject_ShouldInsertOneRecord ()
+        {
+            // Arrange
+            var connectionString = ConfigurationManager.ConnectionStrings["test"].ToString();
+            var adapter = new TTTDataAdapter(connectionString);
+            var newGame = new TicTacToeData();
+            int numPreExistingGames;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var queryString = "SELECT COUNT(*) FROM GameData";
+                var command = new SqlCommand(queryString, connection);
+
+                connection.Open();
+                numPreExistingGames = (int)command.ExecuteScalar();
+            }
+
+            // Act
+            adapter.Save(newGame);
+
+            // Assert
+            int numPostExistingGames;
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var queryString = "SELECT COUNT(*) FROM GameData";
+                var command = new SqlCommand(queryString, connection);
+
+                connection.Open();
+                numPostExistingGames = (int)command.ExecuteScalar();
+            }
+
+            Assert.AreEqual(numPreExistingGames + 1, numPostExistingGames);
+        }
+
+        [TestMethod, TestCategory("IntegrationTest")]
+        public void SavingExistingObject_ShouldPreserveNumberOfRecords ()
+        {
+            // Arrange
+
+            // create new game and insert it, so we have something to update
+            var gameToUpdate = new TicTacToeData();
+            var connectionString = ConfigurationManager.ConnectionStrings["test"].ToString();
+            var adapter = new TTTDataAdapter(connectionString);
+
+            adapter.Save(gameToUpdate);
+
+            int numPreExistingGames;
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var queryString = "SELECT COUNT(*) FROM GameData";
+                var command = new SqlCommand(queryString, connection);
+
+                connection.Open();
+                numPreExistingGames = (int)command.ExecuteScalar();
+            }
+
+            // Act
+            gameToUpdate.CurrentPlayer = Player.O;
+            adapter.Save(gameToUpdate);
+
+            // Assert
+            int numPostExistingGames;
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var queryString = "SELECT COUNT(*) FROM GameData";
+                var command = new SqlCommand(queryString, connection);
+
+                connection.Open();
+                numPostExistingGames = (int)command.ExecuteScalar();
+            }
+            Assert.AreEqual(numPreExistingGames, numPostExistingGames);
+        }
+
+        [TestMethod, TestCategory("IntegrationTest")]
+        public void DeletingGame_ShouldRemoveOneRecord ()
+        {
+            // Arrange
+
+            // create new game and insert it, so we have something to delete
+            var gameToUpdate = new TicTacToeData();
+            var connectionString = ConfigurationManager.ConnectionStrings["test"].ToString();
+            var adapter = new TTTDataAdapter(connectionString);
+
+            adapter.Save(gameToUpdate);
+
+            int numPreExistingGames;
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var queryString = "SELECT COUNT(*) FROM GameData";
+                var command = new SqlCommand(queryString, connection);
+
+                connection.Open();
+                numPreExistingGames = (int)command.ExecuteScalar();
+            }
+
+            // Act
+            adapter.Delete(gameToUpdate.Id);
+
+            // Assert
+            int numPostExistingGames;
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var queryString = "SELECT COUNT(*) FROM GameData";
+                var command = new SqlCommand(queryString, connection);
+
+                connection.Open();
+                numPostExistingGames = (int)command.ExecuteScalar();
+            }
+            Assert.AreEqual(numPreExistingGames - 1, numPostExistingGames);
         }
     }
 }
