660166e395c44544ae18eef1f54d962587ea2a0f
Dylan Sprague <dsprague@docuphase.com>
2018-12-31 11:22:33 -0500

    Create GameController with one route, build out Web API project setup/config

---
+++ NTicTacToe/Controllers/GameController.cs
@@ -0,0 +1,28 @@
+﻿using Interfaces;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Web.Http;
+
+namespace NTicTacToe.Controllers
+{
+    [RoutePrefix("api/v0.1")]
+    public class GameController : ApiController
+    {
+        public IDataManager Manager { get; private set; }
+
+        public GameController(IDataManager dataManager)
+        {
+            Manager = dataManager;
+        }
+
+        [HttpGet]
+        [Route("games")]
+        public IHttpActionResult GetAllGames()
+        {
+            var allGames = Manager.GetAllGamesData();
+            var allGameIds = allGames.Select(game => game.Id);
+            var json = JToken.FromObject(allGameIds);
+            return Ok(json);
+        }
+    }
+}
--- NTicTacToe/Global.asax.cs
+++ NTicTacToe/Global.asax.cs
@@ -1,5 +1,12 @@
-﻿using System;
+﻿using Adapters;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Interfaces;
+using Managers;
+using NTicTacToe.Controllers;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -11,7 +18,30 @@ namespace NTicTacToe
     {
         protected void Application_Start()
         {
+            var diContainer = BuildContainer();
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(diContainer);
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+
+        private IContainer BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            // controllers 
+            builder.RegisterType<GameController>().InstancePerRequest();
+
+            builder.RegisterType<DataManager>().As<IDataManager>().InstancePerLifetimeScope();
+            builder.RegisterType<GameManagerFactory>().As<IGameManagerFactory>().InstancePerLifetimeScope();
+
+            // data adapters
+            var connectionString = ConfigurationManager.ConnectionStrings["sql-dev"].ToString();
+            builder.Register(x =>
+            {
+                return new SqlServerAdapter(connectionString);
+            }).As<IGenericDataAdapter<ITicTacToeData>>().InstancePerLifetimeScope();
+
+            return builder.Build();
         }
     }
 }
